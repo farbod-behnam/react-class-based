b8fb7dc8da7b7e41cfe295d6e608c5b62700828c
farbod-behnam <farbod.behnam@gmail.com>
2023-02-02 13:23:58 +0330

    Convert functional User to class-based one

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,13 +1,21 @@
 
+import { Component } from "react";
 import classes from "./User.module.css";
 
 interface Props {
     name: string;
 }
 
-export default function User(props: Props) {
+export default class User extends Component<Props> {
 
-    return (
-        <li className={classes.user}>{props.name}</li>
-    );
+    render() {
+        return <li className={classes.user}>{this.props.name}</li>
+    }
 }
+
+// export default function User(props: Props) {
+
+//     return (
+//         <li className={classes.user}>{props.name}</li>
+//     );
+// }
